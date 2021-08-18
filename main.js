a764c635a10735f95937df756a005b5e22bc3ce8
PrintRonald <madariaga492@gmail.com>
2021-08-18 03:05:22 -0400

    9
    9

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ function changeView(type) {
 document.addEventListener('DOMContentLoaded', () => {
   const elementosCarousel = document.querySelectorAll('.carousel');
   M.Carousel.init(elementosCarousel, {
-      duration: 1500,
+      duration: 150,
       dist:-80,
       shift: 5,
       padding: 5,
