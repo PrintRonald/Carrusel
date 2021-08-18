1ed9cc14646cfc419a3434ce0beb3ecedf19b567
PrintRonald <madariaga492@gmail.com>
2021-08-18 02:11:47 -0400

    2
    2

--- main.js
+++ main.js
@@ -2,21 +2,21 @@ function changeView(type) {
   const path = window.location.pathname;
   console.log(path);
   if (type === "home") {
-    if (path === '/index.html') {
+    if (path === 'carrusel/index.html') {
       document.body.style.backgroundColor = "red";
       return false;
     } else {
-      window.location.href = "/index.html";
+      window.location.href = "carrusel/index.html";
     }
   } else if (type === "register") {
     if (path === '/register.html') {
       document.body.style.backgroundColor = "green";
       return false;
     } else {
-      window.location.href = "/register.html";
+      window.location.href = "carrusel/register.html";
     }
   } else {
-    window.location.href = "/routes.html";
+    window.location.href = "carrusel/routes.html";
 
   }
 }
